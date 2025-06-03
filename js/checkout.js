0a967eccacbf6b858a3b378fb127e911761256b0
Carla Lopez <lpz.carla12@gmail.com>
2025-06-03 16:05:20 -0300

    se agregaron funcionalidades para el carrito

--- js/checkout.js
+++ js/checkout.js
@@ -12,3 +12,65 @@
 // FUNCIÓN PRINCIPAL
 
 // EVENTOS
+
+
+// VARIABLES
+const carrito = []
+
+const precioTotal = document.querySelector('table tfoot td#totalPrice span')
+const btnComprar = document.querySelector('button#btnBuy')
+const btnRetornar = document.querySelector('button#btnReturn')
+const tableBody = document.querySelector('table tbody#tableBody')
+
+// LÓGICA
+function recuperarCarrito() {
+    const recuperarCarrito = JSON.parse(localStorage.getItem('shoppingKart'))
+
+    if (Array.isArray(recuperarCarrito)) {
+        carrito.push(...recuperarCarrito)
+    }
+}
+
+function calcularTotalCarrito() {
+    if (carrito.length > 0) {
+        precioTotal.textContent = carrito.reduce((acc, prod)=> acc + prod.precio, 0).toFixed(2) || 0.00
+    }
+}
+
+function crearFilaCarrito(prod) {
+    return `<tr>
+                <td id="pImagen">${prod.imagen}</td>
+                <td id="nombre">${prod.nombre}</td>
+                <td id="price">$ ${prod.precio.toFixed(2)}</td>
+                <td id="delButton" 
+                    data-codigo="${prod.id}"
+                    title="Clic para eliminar">
+                    ⛔️
+                </td>
+            </tr>`
+}
+
+function mostrarCarrito() {
+    if (carrito.length > 0) {
+        tableBody.innerHTML = ''
+        carrito.forEach((prod)=> {
+            tableBody.innerHTML += crearFilaCarrito(prod)
+        })
+        calcularTotalCarrito()
+        btnComprar.removeAttribute('disabled')
+    }
+}
+
+// FUNCIÓN PRINCIPAL
+recuperarCarrito()
+mostrarCarrito()
+
+// EVENTOS
+btnRetornar.addEventListener('click', ()=> location.href = 'index.html')
+
+btnComprar.addEventListener('click', ()=> {
+    alert('🛍️ Compra finalizada. Muchas gracias!')
+    localStorage.removeItem('shoppingKart')
+    carrito.length = 0
+    setTimeout(() => btnRetornar.click(), 2500)
+})
